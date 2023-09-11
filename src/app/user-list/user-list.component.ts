e5b070551f1d21de2c8db473c61dad89207dbe78
William Almeida <will.allmeida@gmail.com>
2023-09-11 16:54:37 -0300

    Fix edit refresh

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -51,7 +51,8 @@ export class UserListComponent implements OnInit {
     
         dialogRef.afterClosed().subscribe(result => {
             if(result.success){
-                this.dataSource = [...this.dataSource, result.data];
+                this.dataSource[i] = result.data;
+                this.myTable.renderRows();
             }
         });
     }
